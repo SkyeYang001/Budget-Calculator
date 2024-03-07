daa0adce429617bca91caaca68ba1540d0d14f3d
SkyeYang001 <143848467+SkyeYang001@users.noreply.github.com>
2024-03-07 14:46:46 -0800

    Nate

--- chart.js
+++ chart.js
@@ -5,15 +5,19 @@ import Chart from './node_modules/chart.js/auto'
         labels: [
           'Red',
           'Blue',
-          'Yellow'
+          'Yellow',
+          'Red1',
+          'Blue2',
+          'Yellow3'
         ],
         datasets: [{
           label: 'My First Dataset',
-          data: [300, 50, 100],
+          data: [300, 50, 100, 10, 20 ,15 ],
           backgroundColor: [
             'rgb(255, 99, 132)',
             'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
+            'rgb(255, 205, 86)',
+            'rgb(0,255,0)'
           ],
           hoverOffset: 4
         }]
