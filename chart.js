99359f6462371b31591779d12994ab681063300d
SkyeYang001 <143848467+SkyeYang001@users.noreply.github.com>
2024-03-08 15:46:05 -0800

    ME
    3/8/24

--- chart.js
+++ chart.js
@@ -3,21 +3,23 @@ import Chart from './node_modules/chart.js/auto'
 (async function() {
     const data = {
         labels: [
-          'Red',
-          'Blue',
-          'Yellow',
-          'Red1',
-          'Blue2',
-          'Yellow3'
+          'Expenses',
+          'Insurance',
+          'Loans',
+          'Transport',
+          'Entertainment',
+          'Taxes'
         ],
         datasets: [{
           label: 'My First Dataset',
-          data: [300, 50, 100, 10, 20 ,15 ],
+          data: [50, 50, 50, 50, 50 ,15 ],
           backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)',
-            'rgb(0,255,0)'
+            '#82B5F2',
+            '#82B5F2',
+            '#82B5F2',
+            '#82B5F2',
+            '#82B5F2',
+            '#82B5F2'
           ],
           hoverOffset: 4
         }]
